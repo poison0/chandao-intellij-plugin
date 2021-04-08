d696939457583acd8659492a22302bf37eeb84e4
poison0 <15145093453@163.com>
2021-04-08 22:35:54 +0800

    添加弹窗样式

--- src/chandao/action/NewWindowsAction.java
+++ src/chandao/action/NewWindowsAction.java
@@ -10,7 +10,7 @@ public class NewWindowsAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        TaskWindow task = new TaskWindow("测试");
+        TaskWindow task = new TaskWindow(" 56846（掌中宝交易MB）（拣货单bug）手机端拣货单跟PC端显示的顺序不一致（微微）（dear艳艳）");
         System.out.println(task);
     }
     public NewWindowsAction() {
--- src/chandao/gui/TaskWindowGui.java
+++ src/chandao/gui/TaskWindowGui.java
@@ -1,21 +1,38 @@
 package chandao.gui;
 
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class TaskWindowGui {
     private JPanel mainPanel;
-    private JPanel titlePanel;
-    private JLabel titleLabel;
     private JPanel taskDescribe;
     private JPanel historyRecord;
+    private JLabel pic;
+    private JTextPane textPane1;
+    private JLabel history;
 
     private void init() {
-        titlePanel.setPreferredSize(new Dimension(300,40));
-        titleLabel.setText(" 56846（掌中宝交易MB）（拣货单bug）手机端拣货单跟PC端显示的顺序不一致（微微）（dear艳艳）");
-        titleLabel.setIcon(AllIcons.FileTypes.Any_type);
+
+        pic.setText("<html>" +
+                    " <div>  " +
+                    "   <img  width=\"900\" src='http://work.ruiyunnet.com/biz/file-read-344282.png' />" +
+                    " </div>" +
+                    " <div>  " +
+                    "   <img  width=\"900\" src='http://work.ruiyunnet.com/biz/file-read-344282.png' />" +
+                    " </div>" +
+                    "</html>");
+
+        textPane1.setEditable(false);
+        textPane1.setText(
+                "   2020-12-24 18:24:33, 由 hyyao 创建。\n" +
+                "   2020-12-25 10:40:18, 由 詹淑英 指派给 吕强鹏。\n" +
+                "   2021-04-08 15:15:35, 由 吕强鹏 指派给 牛顺顺。\n"
+                );
     }
     public TaskWindowGui() {
         init();
